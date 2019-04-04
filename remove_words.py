e36db35febd7e27a956438d684bf7ec06222b657
Yuan Luo <yuan.hypnos.luo@gmail.com>
2019-04-03 22:49:38 -0500

    pass

--- remove_words.py
+++ remove_words.py
@@ -6,27 +6,16 @@ from utils import clean_str, loadWord2Vec
 import sys
 
 if len(sys.argv) != 2:
-	sys.exit("Use: python remove_words.py <dataset>")
+        sys.exit("Use: python remove_words.py <dataset>")
 
-datasets = ['20ng', 'R8', 'R52', 'ohsumed', 'mr', 'aclImdb', 'ag_news', 'dbpedia', 'ohsumed_5', 'ohsumed_3']
 dataset = sys.argv[1]
-
-if dataset not in datasets:
-	sys.exit("wrong dataset name")
 
 # nltk.download('stopwords')
 stop_words = set(stopwords.words('english'))
 print(stop_words)
 
-# Read Word Vectors
-# word_vector_file = 'data/glove.6B/glove.6B.200d.txt'
-# vocab, embd, word_vector_map = loadWord2Vec(word_vector_file)
-# word_embeddings_dim = len(embd[0])
-# dataset = '20ng'
-
 doc_content_list = []
 f = open('data/corpus/' + dataset + '.txt', 'rb')
-# f = open('data/wiki_long_abstracts_en_text.txt', 'r')
 for line in f.readlines():
     doc_content_list.append(line.strip().decode('latin1'))
 f.close()
@@ -49,31 +38,25 @@ for doc_content in doc_content_list:
     words = temp.split()
     doc_words = []
     for word in words:
-        # word not in stop_words and word_freq[word] >= 5
         if dataset == 'mr':
             doc_words.append(word)
         elif word not in stop_words and word_freq[word] >= 5:
             doc_words.append(word)
 
     doc_str = ' '.join(doc_words).strip()
-    #if doc_str == '':
-        #doc_str = temp
     clean_docs.append(doc_str)
 
 clean_corpus_str = '\n'.join(clean_docs)
 
 f = open('data/corpus/' + dataset + '.clean.txt', 'w')
-#f = open('data/wiki_long_abstracts_en_text.clean.txt', 'w')
 f.write(clean_corpus_str)
 f.close()
 
-#dataset = '20ng'
 min_len = 10000
 aver_len = 0
 max_len = 0 
 
 f = open('data/corpus/' + dataset + '.clean.txt', 'r')
-#f = open('data/wiki_long_abstracts_en_text.txt', 'r')
 lines = f.readlines()
 for line in lines:
     line = line.strip()
